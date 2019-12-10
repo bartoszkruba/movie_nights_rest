774225848c1ba5684e3bb0ffe8f4be2ac6b21abd
bartoszkruba <bartosz.kruba@gmail.com>
2019-12-10 15:23:46 +0100

    UserRepository bugfix

--- src/main/java/com/example/movie_nights_rest/repository/UserRepository.java
+++ src/main/java/com/example/movie_nights_rest/repository/UserRepository.java
@@ -13,6 +13,4 @@ public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
 
     Boolean existsByEmail(String email);
-
-    Optional<User> findByUsername(String username);
 }
